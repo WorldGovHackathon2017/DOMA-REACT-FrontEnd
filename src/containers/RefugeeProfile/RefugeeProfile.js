b98c874c32d22342332e3913282ffefa5de513fe
yomoda07 <kanji.yy@gmail.com>
2017-12-25 23:32:58 -0500

    Implement refugee profile

--- src/containers/RefugeeProfile/RefugeeProfile.js
+++ src/containers/RefugeeProfile/RefugeeProfile.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
-import {
-  Card,
-  CardActions,
-  CardHeader,
-  CardMedia,
-  CardTitle,
-  CardText
-} from 'material-ui/Card';
+
 import {GridList, GridTile} from 'material-ui/GridList';
 import Divider from 'material-ui/Divider';
 import FlatButton from 'material-ui/FlatButton';
-import { MdPermIdentity, MdPeopleOutline, MdSchool } from 'react-icons/lib/md';
+import {
+  MdPermIdentity,
+  MdPeopleOutline,
+  MdSchool,
+  MdLocationSearching,
+  MdLocationOn,
+  MdPhone,
+  MdEmail
+} from 'react-icons/lib/md';
 import './RefugeeProfile.css';
 import { fetchRefugee } from '../../actions/refugee';
 
@@ -24,7 +25,7 @@ class RefugeeProfile extends Component {
     this.state = {
       refugee: {
         images: [
-          'https://via.placeholder.com/1040x400', 'https://via.placeholder.com/1040x400', 'https://via.placeholder.com/1040x400', 'https://via.placeholder.com/1040x400'
+          'https://via.placeholder.com/900x400', 'https://via.placeholder.com/900x400', 'https://via.placeholder.com/900x400', 'https://via.placeholder.com/900x400'
         ],
         about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.',
         first_name: 'John',
@@ -68,61 +69,47 @@ class RefugeeProfile extends Component {
   }
 
   renderLocation() {
-    const {city, state, country} = this.state.refugee;
-    return `${city}, ${state}, ${country}`;
+    const {current_city, current_state, current_country} = this.state.refugee;
+    return `${current_city}, ${current_state}`;
   }
 
   render() {
     const sliderSettings = {
       dots: true,
-      arrows: true,
+      arrows: false,
       infinite: true,
-      speed: 500,
+      speed: 200,
       slidesToShow: 1,
       slidesToScroll: 1
     };
     return (
-      <div className="refugee-profile">
-        <Card>
+      <div>
+        <section className='carousel-container'>
           <Slider {...sliderSettings}>
             {this.state.refugee.images.map((imgUrl, i) =>
-              <div key={i}><img src={imgUrl} style={{ width: '100%' }} /></div>
+              <div key={i}><img src={imgUrl} style={{ width: '100%', minHeight: 220 }} /></div>
             )}
           </Slider>
-          <div class='section-container'>
-            <CardTitle
-              title={this.renderFullName()}
-              subtitle={this.renderLocation()}
-            />
-            <GridList cols={2} padding={1}>
-              <GridTile>
-                <CardText style={{ textAlign: 'left'}}>
-                  <h2>About</h2>
-                  <p>{this.state.refugee.about}</p>
-                </CardText>
-              </GridTile>
-              <GridTile>
-                <h2>Contact</h2>
-              </GridTile>
-            </GridList>
+        </section>
+        <section className='profile-container main-background-color'>
+          <div className='profile-header'>Profile Hub</div>
+          <div className='profile-body'>
+            <MdLocationSearching size={80} />
+            <div className='name-container'>
+              <h2 className='name'>{this.renderFullName()}</h2>
+              <MdLocationOn size={26} className='accent-color' style={{ verticalAlign: 'bottom', marginLeft: -5}} />{this.renderLocation()}
+            </div>
           </div>
-          <GridList
-            cols={2}
-            cellHeight={400}
-            padding={1}
-          >
-            <GridTile>
-              <CardText style={{ textAlign: 'left'}}>
-                <h2>Request</h2>
-                <div>Number of guests</div>
-                { this.state.request.number_of_guests === 1 ? <MdPermIdentity size={30}/> : <MdPeopleOutline size={30} />} {this.state.request.number_of_guests}
-              </CardText>
-            </GridTile>
-            <GridTile>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100939.985551019!2d-122.50764018567142!3d37.75781499658446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2z44Ki44Oh44Oq44Kr5ZCI6KGG5Zu9IOOCq-ODquODleOCqeODq-ODi-OCouW3niDjgrXjg7Pjg5Xjg6njg7PjgrfjgrnjgrM!5e0!3m2!1sja!2sca!4v1513817160681" width="100%" height="400" frameBorder="0" style={{ border: 0}} allowFullScreen></iframe>
-            </GridTile>
-          </GridList>
-        </Card>
+        </section>
+        <section className='profile-section'>
+          <h3 className='accent-color'>ABOUT ME</h3>
+          <p>{this.state.refugee.about}</p>
+        </section>
+        <section className='profile-section'>
+          <h3 className='accent-color'>CONTACT INFO</h3>
+          <MdPhone className='contact main-color' size={30} />
+           <MdEmail className='contact main-color' size={30} />
+        </section>
       </div>
     )
   }
